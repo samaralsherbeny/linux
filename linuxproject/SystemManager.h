fbaf8247e226b6fe68db49eb45c4d738b370926c
samaralsherbeny <samaralsherbeny2@gmail.com>
2026-08-16 13:39:28 +0300

    Create SystemManager.h

---
+++ linuxproject/SystemManager.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class SystemManager {
+public:
+    void showSystemInfo() const;
+    void showOSInfo() const;
+    void showMemoryInfo() const;
+    void showDiskInfo() const;
+    void showCPUInfo() const;
+};
